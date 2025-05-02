0398ad114347c98108c8a7ba5c95fd853e075721
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-05-02 17:55:13 +0100

    creating the recipe list component for showing all the recipes on the data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'list', component: RecipeListComponent}
 ];
